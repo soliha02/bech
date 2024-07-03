e377913b92c06b302e9c594bc16d5e09973d8d94
soliha02 <164037162+soliha02@users.noreply.github.com>
2024-07-03 10:56:39 +0500

    Add files via upload

---
+++ negr.js
@@ -0,0 +1,18 @@
+let random = document.getElementById('h1')
+
+console.log(random);
+
+random.style.backgroundColor = 'black'
+random.style.color = 'white'
+random.style.textAlign = 'center'
+random.style.borderRadius = '10pxz'
+
+let rasm = document.getElementById('rasm')
+
+rasm.src = 'https://thumbs.dreamstime.com/b/%D0%BB%D0%BE%D0%B3%D0%BE%D1%82%D0%B8%D0%BF-%D0%B7%D0%BD%D0%B0%D1%87%D0%BA%D0%B0-javascript-%D1%87%D0%B0%D1%81%D1%82%D0%BE-%D1%81%D0%BE%D0%B1%D0%BB%D0%B0%D0%B7%D0%BD%D0%B8%D0%BB-%D0%BF%D0%BE-%D0%BC%D0%B5%D1%80%D0%B5-%D1%82%D0%BE%D0%B3%D0%BE-%D0%BA%D0%B0%D0%BA-js-%D1%8F%D0%B7%D1%8B%D0%BA-204759326.jpg'
+
+rasm.style.width = '200px'
+rasm.style.marginLeft = '650px'
+
+
+
